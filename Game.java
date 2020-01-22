920493139613cda9b6312be873e7214587d75f2b
sathvikhussey <60115678+sathvikhussey@users.noreply.github.com>
2020-01-22 17:54:51 +0530

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,9 @@
+public class Game {
+		Player player;
+		int n; // Number of players variable
+		int roundCount;
+		// array list communal pile
+		// game statistics
+			
+		// Method: Split the cards 
+}
---
+++ Player.java
@@ -0,0 +1,6 @@
+public class Player {
+	String name;
+	// Card Array List: Cards[] deck
+	int n; // Length of deck
+	boolean isWinner; 
+}
