e10f853da358e936afda054dfc594854397c1781
sathvikhussey <60115678+sathvikhussey@users.noreply.github.com>
2020-01-28 23:19:19 +0530

    Update Dealer.java

--- MScIT_TeamProject_TemplateProject/src/Dealer.java
+++ MScIT_TeamProject_TemplateProject/src/Dealer.java
@@ -73,14 +73,14 @@ public class Dealer
 		private static String convertString(String filePath) 
 		{
 			String content = "";
-        try
-        {
-            content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
-        } 
-        catch (IOException e) 
-        {
-            e.printStackTrace();
-        }
-        return content;
+        	try
+        	{
+            		content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
+        	} 
+        	catch (IOException e) 
+        	{
+            		e.printStackTrace();
+        	}
+        		return content;
 		}
 }
